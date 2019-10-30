a04b0a4dc4313050ec0ee6e30bd3d84860298751
gsmlg <me@gsmlg.org>
2019-10-30 18:03:45 +0800

    update

--- get_release.sh
+++ get_release.sh
@@ -4,16 +4,23 @@ FILE=$(curl -sSL https://storage.googleapis.com/tekton-releases/pipeline/latest/
 
 IMAGES=$( echo "${FILE}" | grep gcr.io |awk '{print $2}')
 
+VAR="v0.0.1"
+
 for img in $IMAGES
 do
     echo $img
-    toimg=$(echo $img | sed 's;gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/;docker.io/gsmlg/pipline-;g')
+    toimgs=$(echo $img | sed 's;gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/;docker.io/gsmlg/pipline-;g')
+    toimg="$(echo $toimgs | awk -F'@' '{print $1}'):$VAR"
     docker pull $img
     docker tag "$img" "$toimg"
     docker push "$toimg"
 done
 
-$UPDATED=$( echo "${FILE}" | sed 's;gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/;docker.io/gsmlg/pipline-;g' )
+#echo "${FILE}" | sed 's;gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/;docker.io/gsmlg/pipline-;g'
 
-echo $UPDATED
+UPDATED=$( echo "${FILE}" | sed 's;gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/;docker.io/gsmlg/pipline-;g' )
+
+echo "$UPDATED" > updated.yaml
+
+echo "$UPDATED"
 
