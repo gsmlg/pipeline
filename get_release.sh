55422b2c1905b9f1ef6cb6a3ef27ffb88116fe0e
gsmlg <me@gsmlg.org>
2019-10-30 18:08:17 +0800

    fix

--- get_release.sh
+++ get_release.sh
@@ -9,16 +9,16 @@ VAR="v0.0.1"
 for img in $IMAGES
 do
     echo $img
-    toimgs=$(echo $img | sed 's;gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/;docker.io/gsmlg/pipline-;g')
+    toimgs=$(echo $img | sed 's;gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/;docker.io/gsmlg/pipeline-;g')
     toimg="$(echo $toimgs | awk -F'@' '{print $1}'):$VAR"
     docker pull $img
     docker tag "$img" "$toimg"
     docker push "$toimg"
 done
 
-#echo "${FILE}" | sed 's;gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/;docker.io/gsmlg/pipline-;g'
+#echo "${FILE}" | sed 's;gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/;docker.io/gsmlg/pipeline-;g'
 
-UPDATED=$( echo "${FILE}" | sed 's;gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/;docker.io/gsmlg/pipline-;g' )
+UPDATED=$( echo "${FILE}" | sed 's;gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/;docker.io/gsmlg/pipeline-;g' )
 
 echo "$UPDATED" > updated.yaml
 
