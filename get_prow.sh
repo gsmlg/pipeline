1e2d727516bb8d6efc9161c3eafbc7343b0401be
gsmlg <me@gsmlg.org>
2020-01-02 14:04:08 +0800

    prow

--- get_prow.sh
+++ get_prow.sh
@@ -9,14 +9,14 @@ IMAGES=$( echo "${FILE}" | grep gcr.io |awk '{print $2}')
 for img in $IMAGES
 do
     echo $img
-    toimg=$(echo $img | sed 's;gcr.io/k8s-prow/;podman.io/gsmlg/k8s-prow-;g')
+    toimg=$(echo $img | sed 's;gcr.io/k8s-prow/;docker.io/gsmlg/k8s-prow-;g')
     #echo $toimg
     podman pull $img
     podman tag "$img" "$toimg"
     podman push "$toimg"
 done
 
-UPDATED=$( echo "${FILE}" | sed 's;gcr.io/k8s-prow/;podman.io/gsmlg/k8s-prow-;g' )
+UPDATED=$( echo "${FILE}" | sed 's;gcr.io/k8s-prow/;docker.io/gsmlg/k8s-prow-;g' )
 
 echo "$UPDATED" > updated_prow.yaml
 
