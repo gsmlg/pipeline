5626029b9488914028f57103b8805f85a8caf98e
gsmlg <me@gsmlg.org>
2020-02-20 16:26:37 +0800

    update dashboard

---
+++ get_yaml.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -sSL https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml -o tekton.yaml
+
+curl -sSL https://github.com/tektoncd/dashboard/releases/download/v0.5.1/tekton-dashboard-release.yaml -o tekton-dashboard.yaml
+
+
+
+
+
