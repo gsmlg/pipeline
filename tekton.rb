52fc5622f78403e8453e653e66c8e094ad06e7ab
gsmlg <me@gsmlg.org>
2020-02-20 18:40:29 +0800

    update

--- tekton.rb
+++ tekton.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/env ruby
 
-#require 'file'
 
 File.open "tekton.yaml" do |f|
   cnt = File.read f
-  #$stdout.print cnt
 
   reg = /gcr.io[\/A-Za-z0-9.:@-]+/
   out = cnt.scan reg
@@ -24,4 +22,31 @@ File.open "tekton.yaml" do |f|
   
   File.write 'updated_tekton.yaml', newCnt
 end
+
+File.open "tekton-dashboard.yaml" do |f|
+  DASHBOARD_VERSION = "v0.5.1"
+  cnt = File.read f
+
+  reg = /gcr.io[\/A-Za-z0-9.:@-]+/
+  out = cnt.scan reg
+
+  newCnt = cnt
+
+  # gcr.io/tekton-releases/github.com/tektoncd/dashboard/cmd/dashboard
+  out.each do |url|
+    puts url
+    newUrl = url.sub(/gcr.io\/tekton-releases\/github.com\/tektoncd\/dashboard.*\/cmd\//, 'docker.io/gsmlg/tekton-dashboard-').sub(/@.+$/, ":#{DASHBOARD_VERSION}")
+    newCnt.gsub!(url, newUrl)
+    puts newUrl
+    puts `docker pull #{url}`
+    puts `docker tag #{url} #{newUrl}`
+    puts `docker push #{newUrl}`
+    `echo #{newUrl} >> tekton-images.txt`
+  end
+  
+  File.write 'updated_tekton.yaml', newCnt
+end
+
+
+
 
